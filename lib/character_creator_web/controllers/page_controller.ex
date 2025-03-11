e544127c8ae069b24665911d0cf09cbfdb55170f
Austin Love <austin.love.business@gmail.com>
2025-03-11 15:39:18 -0400

    changed file names

--- lib/character_creator_web/controllers/page_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule ElixirTemplateWeb.PageController do
-  use ElixirTemplateWeb, :controller
-
-  def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
-  end
-end
